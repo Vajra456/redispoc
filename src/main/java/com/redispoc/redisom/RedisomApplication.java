0e1139022caf5a98de46ce0b27415c8bdb375a51
apple <apple@ankalans-MacBook-Pro.local>
2022-08-24 18:44:52 +0530

    initial commit

---
+++ src/main/java/com/redispoc/redisom/RedisomApplication.java
@@ -0,0 +1,13 @@
+package com.redispoc.redisom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisomApplication.class, args);
+	}
+
+}
